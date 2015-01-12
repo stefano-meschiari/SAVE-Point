d17e293c43dcb37aa4310a5a29000bdd4585a6c8
Stefano Meschiari <stefano.meschiari@gmail.com>
2015-01-12 11:51:45 -0600

    Added map.yaml; started rendered in map.js

---
+++ apps/gravity/js/map.js
@@ -0,0 +1,15 @@
+/*
+ * Reads in the map.yaml and transforms it into linear arrays.
+ */
+
+"use strict";
+
+
+var AppMenuView = Backbone.View.extend({
+    // Top-level container
+    el: $("#app-menu"),
+
+    initialize: function() {
+        
+    }
+});
