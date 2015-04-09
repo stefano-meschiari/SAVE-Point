326fcab0793a083811343ef4b9c4b7148efa1188
stefano-m1 <stefano.meschiari@gmail.com>
2015-04-09 13:25:43 -0500

    Added Irina, GravityKit, and new description of our activities

--- apps/orbits/js/app.js
+++ apps/orbits/js/app.js
@@ -1783,3 +1783,12 @@ $(window).load(function() {
 
 if (_.parameter('mission') != null)
     $('html').addClass(_.parameter('mission'));
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
